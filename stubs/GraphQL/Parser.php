e35adacbb4c9996e07776c3a2eef9150114c26d9
Andreas Heiberg <andreas@heiberg.io>
2017-02-11 23:16:55 +0000

    add php stubs for IDE support

---
+++ stubs/GraphQL/Parser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GraphQL;
+
+class Parser
+{
+    /**
+     * @param string $input
+     *
+     * @return array
+     */
+    public function parse(string $input): array {
+        return [];
+    }
+}
